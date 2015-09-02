8f64ae0fab1761043ab9844ad191246a2c4c5585
Haifeng Li <haifeng.li@adp.com>
2015-09-02 18:08:58 -0400

    update license header

--- json/src/test/scala/unicorn/json/CompactPrinterSpec.scala
+++ json/src/test/scala/unicorn/json/CompactPrinterSpec.scala
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * (C) Copyright 2015 ADP, LLC.
+ *   
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *  
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
 package unicorn.json
 
 import org.specs2.mutable._
@@ -33,7 +49,7 @@ class CompactPrinterSpec extends Specification {
       CompactPrinter(JsString("xyz")) mustEqual "\"xyz\""
     }
     "properly escape special chars in JsString" in {
-      CompactPrinter(JsString("\"\\\b\f\n\r\t")) mustEqual """"\"\\\b\f\n\r\t""""
+      CompactPrinter(JsString("\"\\\b\f\n\r\t")) mustEqual """"\"\\\b\f\n\r\t"""
       CompactPrinter(JsString("\u1000")) mustEqual "\"\u1000\""
       CompactPrinter(JsString("\u0100")) mustEqual "\"\u0100\""
       CompactPrinter(JsString("\u0010")) mustEqual "\"\\u0010\""
