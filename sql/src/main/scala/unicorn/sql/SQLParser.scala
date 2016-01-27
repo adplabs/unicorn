19dfdf027ad9886a6ce1417929076f87bb4ebdca
Haifeng Li <haifeng.li@adp.com>
2016-01-27 17:25:19 -0500

    dot notation

--- sql/src/main/scala/unicorn/sql/SQLParser.scala
+++ sql/src/main/scala/unicorn/sql/SQLParser.scala
@@ -1,11 +1,10 @@
 package unicorn.sql
 
 import scala.util.matching.Regex
-import scala.util.parsing.combinator.RegexParsers
-import scala.util.parsing.combinator._
+//import scala.util.parsing.combinator._
 import scala.util.parsing.combinator.lexical._
 import scala.util.parsing.combinator.syntactical._
-import scala.util.parsing.combinator.token._
+//import scala.util.parsing.combinator.token._
 import scala.util.parsing.input.CharArrayReader.EofCh
 
 /** A simple SQL parser.
@@ -22,15 +21,18 @@ object SQLParser extends StandardTokenParsers {
     }
   }
 
-  def parseSelectStatement: Parser[SelectStatement] = (
-    "SELECT" ~> parseProjections ~ "FROM" ~ parseRelations ~ parseWhere.? ~ parseGroupBy.? ~ parseOrderBy.? ~ parseLimit.? <~ ";".? ^^ { case pro ~ _ ~ tab ~ whe ~ grp ~ ord ~ lim => SelectStatement(pro, tab, whe, grp, ord, lim) })
+  def parseSelectStatement: Parser[SelectStatement] = {
+    "SELECT" ~> parseProjections ~ "FROM" ~ parseRelations ~ parseWhere.? ~ parseGroupBy.? ~ parseOrderBy.? ~ parseLimit.? <~ ";".? ^^ { case pro ~ _ ~ tab ~ whe ~ grp ~ ord ~ lim => SelectStatement(pro, tab, whe, grp, ord, lim) }
+  }
 
-  def parseProjections: Parser[Projections] = (
-    "*" ^^^ AllColumns()
-      | rep1sep(parseAliasedExpression, ",") ^^ { case lst => ExpressionProjections(lst) })
+  def parseProjections: Parser[Projections] = {
+    "*" ^^^ AllColumns() |
+    rep1sep(parseAliasedExpression, ",") ^^ { case lst => ExpressionProjections(lst) }
+  }
 
-  def parseAliasedExpression: Parser[(Expression, Option[String])] = (
-    parseExpression ~ ("AS" ~> ident).? ^^ { case expr ~ alias => (expr, alias) })
+  def parseAliasedExpression: Parser[(Expression, Option[String])] = {
+    parseExpression ~ ("AS" ~> ident).? ^^ { case expr ~ alias => (expr, alias) }
+  }
 
   def parseExpression: Parser[Expression] = parseOr
 
@@ -40,141 +42,187 @@ object SQLParser extends StandardTokenParsers {
   def parseAnd: Parser[Expression] =
     parseSimpleExpression * ("AND" ^^^ { (a: Expression, b: Expression) => And(a, b) })
 
-  def parseSimpleExpression: Parser[Expression] = (
+  def parseSimpleExpression: Parser[Expression] = {
     parseAddition ~ rep(
       ("=" | "<>" | "!=" | "<" | "<=" | ">" | ">=") ~ parseAddition ^^ {
         case op ~ right => (op, right)
-      }
-        | "BETWEEN" ~ parseAddition ~ "AND" ~ parseAddition ^^ {
+      } |
+      "BETWEEN" ~ parseAddition ~ "AND" ~ parseAddition ^^ {
         case op ~ a ~ _ ~ b => (op, a, b)
-      }
-        | "NOT" ~> "IN" ~ "(" ~ (parseSelectStatement | rep1sep(parseExpression, ",")) ~ ")" ^^ {
+      } |
+      "NOT" ~> "IN" ~ "(" ~ (parseSelectStatement | rep1sep(parseExpression, ",")) ~ ")" ^^ {
         case op ~ _ ~ a ~ _ => (op, a, true)
-      }
-        | "IN" ~ "(" ~ (parseSelectStatement | rep1sep(parseExpression, ",")) ~ ")" ^^ {
+      } |
+      "IN" ~ "(" ~ (parseSelectStatement | rep1sep(parseExpression, ",")) ~ ")" ^^ {
         case op ~ _ ~ a ~ _ => (op, a, false)
+      } |
+      "NOT" ~> "LIKE" ~ parseAddition ^^ { case op ~ a => (op, a, true) } |
+      "LIKE" ~ parseAddition ^^ { case op ~ a => (op, a, false) }) ^^ {
+    case left ~ elems =>
+      elems.foldLeft(left) {
+        case (acc, (("=", right: Expression)))                  => Equals(acc, right)
+        case (acc, (("<>", right: Expression)))                 => NotEquals(acc, right)
+        case (acc, (("!=", right: Expression)))                 => NotEquals(acc, right)
+        case (acc, (("<", right: Expression)))                  => LessThan(acc, right)
+        case (acc, (("<=", right: Expression)))                 => LessOrEqual(acc, right)
+        case (acc, ((">", right: Expression)))                  => GreaterThan(acc, right)
+        case (acc, ((">=", right: Expression)))                 => GreaterOrEqual(acc, right)
+        case (acc, (("BETWEEN", l: Expression, r: Expression))) => And(GreaterOrEqual(acc, l), LessOrEqual(acc, r))
+        case (acc, (("IN", e: Seq[_], n: Boolean)))             => In(acc, e.asInstanceOf[Seq[Expression]], n)
+        case (acc, (("IN", s: SelectStatement, n: Boolean)))    => In(acc, Seq(s), n)
+        case (acc, (("LIKE", e: Expression, n: Boolean)))       => Like(acc, e, n)
       }
-        | "NOT" ~> "LIKE" ~ parseAddition ^^ { case op ~ a => (op, a, true) }
-        | "LIKE" ~ parseAddition ^^ { case op ~ a => (op, a, false) }) ^^ {
-      case left ~ elems =>
-        elems.foldLeft(left) {
-          case (acc, (("=", right: Expression)))                  => Equals(acc, right)
-          case (acc, (("<>", right: Expression)))                 => NotEquals(acc, right)
-          case (acc, (("!=", right: Expression)))                 => NotEquals(acc, right)
-          case (acc, (("<", right: Expression)))                  => LessThan(acc, right)
-          case (acc, (("<=", right: Expression)))                 => LessOrEqual(acc, right)
-          case (acc, ((">", right: Expression)))                  => GreaterThan(acc, right)
-          case (acc, ((">=", right: Expression)))                 => GreaterOrEqual(acc, right)
-          case (acc, (("BETWEEN", l: Expression, r: Expression))) => And(GreaterOrEqual(acc, l), LessOrEqual(acc, r))
-          case (acc, (("IN", e: Seq[_], n: Boolean)))             => In(acc, e.asInstanceOf[Seq[Expression]], n)
-          case (acc, (("IN", s: SelectStatement, n: Boolean)))    => In(acc, Seq(s), n)
-          case (acc, (("LIKE", e: Expression, n: Boolean)))       => Like(acc, e, n)
-        }
     } |
-      "NOT" ~> parseSimpleExpression ^^ (Not(_)) |
-      "EXISTS" ~> "(" ~> parseSelectStatement <~ ")" ^^ { case s => Exists(s) })
+    "NOT" ~> parseSimpleExpression ^^ (Not(_)) |
+    "EXISTS" ~> "(" ~> parseSelectStatement <~ ")" ^^ { case s => Exists(s) }
+  }
 
   def parseAddition: Parser[Expression] =
     parseMultiplication * (
       "+" ^^^ { (a: Expression, b: Expression) => Add(a, b) } |
-        "-" ^^^ { (a: Expression, b: Expression) => Subtract(a, b) })
+      "-" ^^^ { (a: Expression, b: Expression) => Subtract(a, b) })
 
   def parseMultiplication: Parser[Expression] =
     parsePrimaryExpression * (
       "*" ^^^ { (a: Expression, b: Expression) => Multiply(a, b) } |
-        "/" ^^^ { (a: Expression, b: Expression) => Divide(a, b) })
+      "/" ^^^ { (a: Expression, b: Expression) => Divide(a, b) })
 
-  def parsePrimaryExpression: Parser[Expression] = (
+  def parseJsonField: Parser[Expression] = {
+    dotNotation |
+    ident ^^ (FieldIdent(None, _))
+  }
+
+  val dotNotation: Parser[Expression] = {
+    (ident <~ ".") ~ ident ~ rep("." ~> ident) ^^ {
+      case i1 ~ i2 ~ rest => FieldIdent(None, i1 + "." + i2 + rest.mkString(".", ".", ""))
+    }
+  }
+
+  def parsePrimaryExpression: Parser[Expression] = {
     parseLiteral |
-      parseKnownFunction |
-      ident ~ opt("." ~> ident | "(" ~> repsep(parseExpression, ",") <~ ")") ^^ {
-        case id ~ None           => FieldIdent(None, id)
-        case a ~ Some(b: String) => FieldIdent(Some(a), b)
-      } |
-      "(" ~> (parseExpression | parseSelectStatement) <~ ")"
-      | "+" ~> parsePrimaryExpression ^^ (UnaryPlus(_))
-      | "-" ~> parsePrimaryExpression ^^ (UnaryMinus(_)))
+    parseKnownFunction |
+    parseJsonField |
+    /*
+    // Use parseJsonField for fields but we cannot tell t.a.b where t is the table name or alias
+    // https://mail-archives.apache.org/mod_mbox/spark-commits/201409.mbox/%3C667f345ef8b349a897f1321426c1a2ec@git.apache.org%3E
+    ident ~ opt("." ~> ident | "(" ~> repsep(parseExpression, ",") <~ ")") ^^ {
+      case id ~ None           => FieldIdent(None, id)
+      case a ~ Some(b: String) => FieldIdent(Some(a), b)
+    } |
+    */
+    "(" ~> (parseExpression | parseSelectStatement) <~ ")" |
+    "+" ~> parsePrimaryExpression ^^ (UnaryPlus(_)) |
+    "-" ~> parsePrimaryExpression ^^ (UnaryMinus(_))
+  }
 
-  def parseKnownFunction: Parser[Expression] = (
-    "COUNT" ~> "(" ~> ("*" ^^^ CountAll() | parseExpression ^^ { case expr => CountExpr(expr) }) <~ ")"
-      | "MIN" ~> "(" ~> parseExpression <~ ")" ^^ (Min(_))
-      | "MAX" ~> "(" ~> parseExpression <~ ")" ^^ (Max(_))
-      | "SUM" ~> "(" ~> parseExpression <~ ")" ^^ (Sum(_))
-      | "AVG" ~> "(" ~> parseExpression <~ ")" ^^ (Avg(_)))
+  def parseKnownFunction: Parser[Expression] = {
+    "COUNT" ~> "(" ~> ("*" ^^^ CountAll() |
+    parseExpression ^^ { case expr => CountExpr(expr) }) <~ ")" |
+    "MIN" ~> "(" ~> parseExpression <~ ")" ^^ (Min(_)) |
+    "MAX" ~> "(" ~> parseExpression <~ ")" ^^ (Max(_)) |
+    "SUM" ~> "(" ~> parseExpression <~ ")" ^^ (Sum(_)) |
+    "AVG" ~> "(" ~> parseExpression <~ ")" ^^ (Avg(_))
+  }
 
-  def parseLiteral: Parser[Expression] = (
-    numericLit ^^ { case i => IntLiteral(i.toInt) }
-      | floatLit ^^ { case f => FloatLiteral(f.toDouble) }
-      | stringLit ^^ { case s => StringLiteral(s) }
-      | "NULL" ^^ { case _ => NullLiteral() }
-      | "DATE" ~> stringLit ^^ { case s => DateLiteral(s) })
+  def parseLiteral: Parser[Expression] = {
+    numericLit ^^ { case i => IntLiteral(i.toInt) } |
+    floatLit ^^ { case f => FloatLiteral(f.toDouble) } |
+    stringLit ^^ { case s => StringLiteral(s) } |
+    "NULL" ^^ { case _ => NullLiteral() } |
+    "DATE" ~> stringLit ^^ { case s => DateLiteral(s) }
+  }
 
   def parseRelations: Parser[Seq[Relation]] = rep1sep(parseRelation, ",")
 
-  def parseRelation: Parser[Relation] = (
-    parseSimpleRelation ~ rep(opt(parseJoinType) ~ "JOIN" ~ parseSimpleRelation ~ "ON" ~ parseExpression ^^
-      { case tpe ~ _ ~ r ~ _ ~ e => (tpe.getOrElse(InnerJoin), r, e) }) ^^ {
-      case r ~ elems => elems.foldLeft(r) { case (x, r) => Join(x, r._2, r._1, r._3) }
-    })
+  def parseRelation: Parser[Relation] = {
+    parseSimpleRelation ~ rep(opt(parseJoinType) ~ "JOIN" ~ parseSimpleRelation ~ "ON" ~ parseExpression ^^ {
+      case tpe ~ _ ~ r ~ _ ~ e => (tpe.getOrElse(InnerJoin), r, e) }) ^^ {
+        case r ~ elems => elems.foldLeft(r) { case (x, r) => Join(x, r._2, r._1, r._3)
+      }
+    }
+  }
 
-  def parseSimpleRelation: Parser[Relation] = (
+  def parseSimpleRelation: Parser[Relation] = {
     ident ~ ("AS".? ~> ident.?) ^^ {
       case tbl ~ alias => Table(tbl, alias)
-    }
-      | ("(" ~> parseSelectStatement <~ ")") ~ ("AS".? ~> ident) ^^ {
+    } |
+    ("(" ~> parseSelectStatement <~ ")") ~ ("AS".? ~> ident) ^^ {
       case subq ~ alias => Subquery(subq, alias)
-    })
+    }
+  }
 
-  def parseJoinType: Parser[JoinType] = (
+  def parseJoinType: Parser[JoinType] = {
     ("LEFT" <~ "OUTER".? | "RIGHT" <~ "OUTER".? | "FULL" ~ "OUTER") ^^ {
       case "LEFT"           => LeftOuterJoin
       case "RIGHT"          => RightOuterJoin
       case "FULL" ~ "OUTER" => FullOuterJoin
-    }
-      | "INNER" ^^^ InnerJoin)
+    } |
+    "INNER" ^^^ InnerJoin
+  }
 
-  def parseWhere: Parser[Expression] = (
-    "WHERE" ~> parseExpression)
+  def parseWhere: Parser[Expression] = {
+    "WHERE" ~> parseExpression
+  }
 
-  def parseGroupBy: Parser[GroupBy] = (
-    "GROUP" ~> "BY" ~> rep1sep(parseExpression, ",") ~ ("HAVING" ~> parseExpression).? ^^
-      { case exp ~ hav => GroupBy(exp, hav) })
+  def parseGroupBy: Parser[GroupBy] = {
+    "GROUP" ~> "BY" ~> rep1sep(parseExpression, ",") ~ ("HAVING" ~> parseExpression).? ^^ { case exp ~ hav => GroupBy(exp, hav) }
+  }
 
-  def parseOrderBy: Parser[OrderBy] = (
-    "ORDER" ~> "BY" ~> rep1sep(parseOrderKey, ",") ^^ { case keys => OrderBy(keys) })
+  def parseOrderBy: Parser[OrderBy] = {
+    "ORDER" ~> "BY" ~> rep1sep(parseOrderKey, ",") ^^ { case keys => OrderBy(keys) }
+  }
 
-  def parseOrderKey: Parser[(Expression, OrderType)] = (
+  def parseOrderKey: Parser[(Expression, OrderType)] = {
     parseExpression ~ ("ASC" | "DESC").? ^^ {
       case v ~ Some("DESC") => (v, DESC)
       case v ~ Some("ASC")  => (v, ASC)
       case v ~ None         => (v, ASC)
-    })
+    }
+  }
 
-  def parseLimit: Parser[Limit] = (
-    "LIMIT" ~> numericLit ^^ { case lim => Limit(lim.toInt) })
+  def parseLimit: Parser[Limit] = {
+    "LIMIT" ~> numericLit ^^ { case lim => Limit(lim.toInt) }
+  }
 
   class SqlLexical extends StdLexical {
     case class FloatLit(chars: String) extends Token {
       override def toString = chars
     }
 
-    // Allow case insensitive keywords by upper casing everything
-    override protected def processIdent(name: String) =
-      if (reserved contains name.toUpperCase) Keyword(name.toUpperCase) else Identifier(name)
+    reserved.clear
+    reserved += (
+      "SELECT", "AS", "OR", "AND", "GROUP", "ORDER", "BY", "WHERE",
+      "JOIN", "ASC", "DESC", "FROM", "ON", "NOT", "HAVING",
+      "EXISTS", "BETWEEN", "LIKE", "IN", "NULL", "LEFT", "RIGHT",
+      "FULL", "OUTER", "INNER", "COUNT", "SUM", "AVG", "MIN", "MAX",
+      "DATE", "TOP", "LIMIT")
 
-    override def token: Parser[Token] =
-      (identChar ~ rep(identChar | digit) ^^ { case first ~ rest => processIdent(first :: rest mkString "") }
-        | rep1(digit) ~ opt('.' ~> rep(digit)) ^^ {
+    delimiters += (
+      "*", "+", "-", "<", "=", "<>", "!=", "<=", ">=", ">", "/", "(", ")", ",", ".", ";")
+
+    /* Normal the keyword string */
+    def normalizeKeyword(str: String): String = str.toUpperCase
+
+    // Allow case insensitive keywords by upper casing everything
+    override protected def processIdent(name: String) = {
+      val token = normalizeKeyword(name)
+      if (reserved contains token) Keyword(token) else Identifier(name)
+    }
+
+    override def token: Parser[Token] = {
+      identChar ~ rep(identChar | digit) ^^ { case first ~ rest => processIdent(first :: rest mkString "") } |
+      rep1(digit) ~ opt('.' ~> rep(digit)) ^^ {
         case i ~ None    => NumericLit(i mkString "")
         case i ~ Some(d) => FloatLit(i.mkString("") + "." + d.mkString(""))
-      }
-        | '\'' ~ rep(chrExcept('\'', '\n', EofCh)) ~ '\'' ^^ { case '\'' ~ chars ~ '\'' => StringLit(chars mkString "") }
-        | '\"' ~ rep(chrExcept('\"', '\n', EofCh)) ~ '\"' ^^ { case '\"' ~ chars ~ '\"' => StringLit(chars mkString "") }
-        | EofCh ^^^ EOF
-        | '\'' ~> failure("unclosed string literal")
-        | '\"' ~> failure("unclosed string literal")
-        | delim
-        | failure("illegal character"))
+      } |
+      '\'' ~ rep(chrExcept('\'', '\n', EofCh)) ~ '\'' ^^ { case '\'' ~ chars ~ '\'' => StringLit(chars mkString "") } |
+      '\"' ~ rep(chrExcept('\"', '\n', EofCh)) ~ '\"' ^^ { case '\"' ~ chars ~ '\"' => StringLit(chars mkString "") } |
+      EofCh ^^^ EOF |
+      '\'' ~> failure("unclosed string literal") |
+      '\"' ~> failure("unclosed string literal") |
+      delim |
+      failure("illegal character")
+    }
 
     def regex(r: Regex): Parser[String] = new Parser[String] {
       def apply(in: Input) = {
@@ -183,8 +231,7 @@ object SQLParser extends StandardTokenParsers {
         val start = offset // handleWhiteSpace(source, offset)
         (r findPrefixMatchOf (source.subSequence(start, source.length))) match {
           case Some(matched) =>
-            Success(source.subSequence(start, start + matched.end).toString,
-              in.drop(start + matched.end - offset))
+            Success(source.subSequence(start, start + matched.end).toString, in.drop(start + matched.end - offset))
           case None =>
             Success("", in)
         }
@@ -196,14 +243,4 @@ object SQLParser extends StandardTokenParsers {
 
   def floatLit: Parser[String] =
     elem("decimal", _.isInstanceOf[lexical.FloatLit]) ^^ (_.chars)
-
-  lexical.reserved += (
-    "SELECT", "AS", "OR", "AND", "GROUP", "ORDER", "BY", "WHERE",
-    "JOIN", "ASC", "DESC", "FROM", "ON", "NOT", "HAVING",
-    "EXISTS", "BETWEEN", "LIKE", "IN", "NULL", "LEFT", "RIGHT",
-    "FULL", "OUTER", "INNER", "COUNT", "SUM", "AVG", "MIN", "MAX",
-    "DATE", "TOP", "LIMIT")
-
-  lexical.delimiters += (
-    "*", "+", "-", "<", "=", "<>", "!=", "<=", ">=", ">", "/", "(", ")", ",", ".", ";")
 }
