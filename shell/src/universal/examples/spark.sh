621541c7b8bd19f3aeee508b1034ee4b589190e4
Haifeng Li <haifeng.li@adp.com>
2016-05-13 16:59:38 -0400

    DataFrame example

--- shell/src/universal/examples/spark.sh
+++ shell/src/universal/examples/spark.sh
@@ -10,12 +10,18 @@ import unicorn.unibase._
 import unicorn.narwhal._
 import unicorn.graph._
 import org.apache.spark._
-//import org.apache.spark.graphx._
+import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 
 val conf = new SparkConf().setAppName("unicorn").setMaster("local[4]")
 val sc = new SparkContext(conf)
 val db = new Narwhal(HBase())
+
+val table = db("worker")
+table.tenant = "IBM"
+val rdd = table.rdd(sc)
+rdd.count()
+
 val table = db("narwhal")
 val rdd = table.rdd(sc, json"""
                           {
@@ -31,7 +37,14 @@ val rdd = table.rdd(sc, json"""
                         """)
 rdd.count()
 
-val table = db("worker")
-table.tenant = "IBM"
-val rdd = table.rdd(sc)
-rdd.count()
+
+val sqlContext = new org.apache.spark.sql.SQLContext(sc)
+import sqlContext.implicits._
+
+case class Worker(name: String, age: Int)
+val workers = rdd.map { js => Worker(js.name, js.age) }
+val df = sqlContext.createDataFrame(workers)
+df.show
+
+df.registerTempTable("worker")
+sqlContext.sql("SELECT * FROM worker WHERE age > 30").show
