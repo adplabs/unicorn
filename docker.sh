fc9b375dad953046949126f5f83f8e6116e4a7e4
Haifeng Li <haifeng.hli@gmail.com>
2016-06-23 13:10:19 -0400

    run latest

--- docker.sh
+++ docker.sh
@@ -7,4 +7,4 @@ data_dir=$PWD/target/data
 mkdir -p $data_dir
 
 
-docker run --name=unicorn-docker -hostname=unicorn-docker --volume=$data_dir:/data -it --rm unicorn:1.2.0
+docker run --name=unicorn-docker -hostname=unicorn-docker --volume=$data_dir:/data -it --rm haifengl/unicorn
